6acececf3855386d7ad397563c88c625ba31cacf
dvesc <75711359+dvesc@users.noreply.github.com>
2020-12-30 13:02:10 -0400

    Add files via upload

---
+++ Parcial N2/JS/class_Invertigador.js
@@ -0,0 +1,142 @@
+/*
+  ape = apellido
+  apli = aplica
+  bts = bits
+  c = casos
+  ci = cedula
+  cod = codificado/s - codifica
+  codi = codificando
+  emp = empleados
+  inv = investigador/es
+  nom = nombre
+  nres= no resueltos
+  rec = record
+  res= resueltos
+  uni/s = unidad/es
+ */
+export default class Investigador {
+  constructor(nom, ape, ci, fecha, unis) {
+    this.nom = this.codNom(nom);
+    this.ape = this.codApe(ape);
+    this.ci = ci;
+    this.fecha = this.codFecha(fecha);
+    this.unis = this.codDatosDeUnis(unis);
+    this.sinCod = {
+      nom, ape, ci,
+      fecha: fecha.split("-").reverse().join("/"),
+      unis
+    };
+  }
+
+  codNom(n) {
+    let nCod = [],
+      trozo1,
+      trozo2,
+      trozo3;
+    n.split("").forEach(char => {
+      (trozo1 = []), (trozo2 = []), (trozo3 = []);
+      char
+        .charCodeAt()
+        .toString(2)
+        .split("")
+        .map((bit, index, arr) =>
+          /[0-2]/.test(index)
+            ? trozo1.push(bit)
+            : new RegExp(`[3-${arr.length - 3}]`).test(index)
+            ? trozo2.push(bit)
+            : trozo3.push(bit)
+        );
+      nCod.push(
+        String.fromCharCode(
+          parseInt([...trozo3, ...trozo2, ...trozo1].join(""), 2)
+        )
+      );
+    });
+    return nCod.join("");
+  } //-------------------------------------------------------------------------
+  codApe(a) {
+    let aCod = [],
+      codi;
+    a.split("").forEach(char => {
+      codi = [];
+      char
+        .charCodeAt()
+        .toString(2)
+        .split("")
+        .map((bit, index) => {
+          if (index !== 0 && index % 2 == 0) bit = `,${bit}`;
+          codi.push(bit);
+        });
+      codi = codi.join("").split(",");
+      for (let i = 0; i < codi.length; i++)
+        if (i % 2 === 0)
+          codi[i] = codi[i]
+            .split("")
+            .reverse()
+            .join("");
+      aCod.push(String.fromCharCode(parseInt(codi.join(""), 2)));
+    });
+    return aCod.join("");
+  } //-------------------------------------------------------------------------
+  codFecha(f) {
+    let fechaCod = f
+      .split("-")
+      .reverse()
+      .map(date => parseInt(date));
+    fechaCod = fechaCod.map((date, index) =>
+      index === 0 
+        ? (date = Math.abs(date-4)) 
+        : index === 1 
+        ? (date = Math.abs(date-3)) 
+        : (date -= 5)
+    );
+    return fechaCod.join("/");
+  } //-------------------------------------------------------------------------
+  apliBts(unis, propiedad) {
+    let cod = [],
+      formateaBts;
+    unis.map((uni, index) => {
+      cod.push(
+        parseInt(
+          propiedad === "emp"
+            ? uni.empleEnSuUni
+            : propiedad === "cRes"
+            ? uni.cRes
+            : propiedad === "cNRes"
+            ? uni.cNRes
+            : uni.nomDeUni
+        ).toString(2)
+      );
+      if (cod[index].length < 4) {
+        formateaBts = cod[index].split("").reverse("");
+        for (let i = formateaBts.length; i <= 3; i++) formateaBts.push(0);
+        cod[index] = formateaBts.reverse("").join("");
+      }
+    });
+    return cod;
+  } //-------------------------------------------------------------------------
+  codDatosDeUnis(u) {
+    let cNRes = [];
+    this.apliBts(u, "cNRes").map((elem, index, arr) => {
+      if (arr.length > 1 && index !== 0 && index % 2 !== 0)
+        cNRes.push([elem, arr[index - 1]].join(""));
+      else cNRes.push(arr[0]);
+    });
+    return [
+      {
+        nomDeUni: parseInt(
+          this.apliBts(u, "nomDeUni")
+            .reverse()
+            .join(""),
+          2),
+        emp: parseInt(
+          this.apliBts(u, "emp")
+            .reverse()
+            .join(""),
+          2),
+        cRes: parseInt(this.apliBts(u, "cRes").join(""), 2),
+        cNRes: parseInt(this.apliBts(u, "cNRes").join(""), 2)
+      }
+    ];
+  }
+}
---
+++ Parcial N2/JS/index.js
@@ -0,0 +1,203 @@
+/* 
+  ape = apellido
+  apli = aplica
+  c = casos
+  ci = cedula
+  cod = codificado/s
+  cont = contiene
+  dec = decodificado/s
+  elem = elemento
+  emp = empleados
+  div/s = <div>/s
+  inp/s = input/s
+  insp = inspeccion
+  inv = investigador/es
+  nom = nombre
+  nres= no resueltos
+  rec = record
+  res= resueltos
+  tab = tabla
+  uni/s = unidad/es
+  val = valor o 'value'
+*/
+import Investigador from "./class_Invertigador.js";
+
+let inpsDeUnis = 1;
+const $d = document,
+  $inpDeNom = $d.querySelector(".inp-de-nom"),
+  $inpDeApe = $d.querySelector(".inp-de-ape"),
+  $inpDeCi = $d.querySelector(".inp-de-ci"),
+  $inpDeFecha = $d.querySelector(".inp-de-fecha"),
+  $contDivsDeUnis = $d.querySelector(".cont-divs-de-unis"),
+  $btnRemInpDeUni = $d.querySelector(".rem-inp-de-uni"),
+  $btnAddInpDeUni = $d.querySelector(".btnAddInpDeUni"),
+  $contTabDeInv = $d.querySelector(".cont-tab-de-inv"),
+  $tabDeInv = $d.querySelector(".tab-de-inv"),
+  $inpDeInsp = $d.querySelector(".inp-de-insp"),
+  $tabDeInsp = $d.querySelector(".tab-de-insp"),
+  arrDeInv = [],
+  apliDisplayASecction = elem => {
+    elem.classList.add("display");
+    elem.nextElementSibling.classList.remove("display");
+  };
+
+$d.addEventListener("DOMContentLoaded", () => {
+  $d.addEventListener("click", e => {
+    if (
+      e.target.matches(".muestra-form-de-inv") ||
+      e.target.matches(".insp-inv") ||
+      e.target.matches(".ver-datos")
+    )
+      apliDisplayASecction(e.target);
+    //-------------------------------------------------------------------------
+    switch (e.target.className) {
+      case "add-inp-de-uni":
+        $btnRemInpDeUni.classList.remove("display");
+        inpsDeUnis++;
+        $contDivsDeUnis.insertAdjacentHTML(
+          "beforeend",
+          `<div class="div-de-uni">
+            <input type="text" 
+              placeholder="Nombre o Distribucion..." 
+              class="nom-de-uni"/>
+            <input type="text" 
+              placeholder="Numero de empleados..." 
+              class="num-de-empleados" 
+              title="Solo se permien numeros / Maximo de Empleados: 15" 
+              pattern="^([1-9]{1}|[1]{1}[0-5]{1})$" required/>
+            <input type="text" 
+              placeholder="N. de casos Resueltos..." 
+              class="c-resueltos" title="Solo se permiten numeros" 
+              pattern="[0-9]{1,}" required/> 
+            <input type="text" 
+              placeholder="N. de casos no Resueltos..." 
+              class="c-no-resueltos" title="Solo se permiten numeros" 
+              pattern="[0-9]{1,}" required/>
+          </div>`
+        );
+        if (inpsDeUnis === 4) e.target.classList.add("display");
+        break; 
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      case "rem-inp-de-uni":
+        inpsDeUnis--;
+        $contDivsDeUnis.removeChild($contDivsDeUnis.lastElementChild);
+        if (inpsDeUnis === 1) e.target.classList.add("display");
+        if (inpsDeUnis === 3) $btnAddInpDeUni.classList.remove("display");
+        break;
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      case "ver-datos-dec":
+      case "ver-datos-cod":
+        let invDecCod;
+        $contTabDeInv.classList.remove("display");
+        $tabDeInv.innerHTML = "";
+        e.target.className === "ver-datos-dec"
+          ? (invDecCod = arrDeInv.map(inv => inv.sinCod))
+          : (invDecCod = arrDeInv);
+        invDecCod.forEach(inv => 
+          $tabDeInv.insertAdjacentHTML(
+            "beforeend",
+            `<tr>
+              <td>${inv.ci}</td>
+              <td>${inv.nom}</td>
+              <td>${inv.ape}</td>
+              <td>${inv.fecha}</td>
+              <td>${
+                e.target.className === "ver-datos-cod"
+                  ? Math.round(Math.random() * (500 - 50 + 1) + 50)
+                  : inv.unis.length
+              }</td>
+            </tr>` 
+          )
+        );
+        break;
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      case "inv-con-rec":
+        let recCRes = 0,
+          invConRec;
+        if (arrDeInv.length > 0) {
+          arrDeInv.forEach(inv => {
+            let cResDelInv = 0;
+            inv.sinCod.unis.forEach(uni => 
+              (cResDelInv = cResDelInv + uni.cRes)
+            );
+            if (cResDelInv >= recCRes) {
+              recCRes = cResDelInv;
+              invConRec = inv.sinCod;
+            }
+          });
+          apliDisplayASecction(e.target);
+          e.target.nextElementSibling.innerHTML = 
+            `<p> 
+              "${invConRec.nom} ${invConRec.ape}" 
+              propietari@ de la Cedula de identidad N.
+              "${invConRec.ci}" posee ${recCRes} casos resueltos
+            </p>
+            <input 
+              type="button" 
+              class="cerrar" 
+              value="Cerrar" 
+            />`
+          ;
+        } else 
+          alert("Aun no tenemos los datos necesarios para esta peticion");
+        break;
+      // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+      case "cerrar":
+        e.target.parentElement.classList.add("display");
+        e.target.parentElement.previousElementSibling.classList.remove(
+          "display"
+        );
+        break;
+    }
+  });
+  //===========================================================================
+  $d.addEventListener("submit", e => {
+    e.preventDefault();
+    if (e.target.matches(".submit-inv")) {
+      let valDeLosImpsDeLasUnis = [];
+      $d.querySelectorAll(".div-de-uni").forEach(divDeUni =>
+        valDeLosImpsDeLasUnis.push({
+          nomDeUni: divDeUni.children[0].value,
+          emp: parseInt(divDeUni.children[1].value),
+          cRes: parseInt(divDeUni.children[2].value),
+          cNRes: parseInt(divDeUni.children[3].value)
+        })
+      );
+      arrDeInv.push(
+        new Investigador(
+          $inpDeNom.value,
+          $inpDeApe.value,
+          $inpDeCi.value,
+          $inpDeFecha.value,
+          valDeLosImpsDeLasUnis
+        )
+      );
+      inpsDeUnis = 1;
+      $btnRemInpDeUni.classList.add("display");
+      $d.querySelectorAll(".div-de-uni").forEach((divDeUni, index) => {
+        if (index > 0) $contDivsDeUnis.removeChild(divDeUni);
+      });
+    }
+    //-------------------------------------------------------------------------
+    else {
+      let invAInsp = arrDeInv.filter(inv => inv.ci === $inpDeInsp.value);
+      $tabDeInsp.innerHTML = "";
+      if (invAInsp.length > 0){
+        apliDisplayASecction(e.target.parentElement);
+        invAInsp[0].sinCod.unis.forEach(uni =>
+          $tabDeInsp.insertAdjacentHTML(
+            "beforeend",
+            `<tr>
+              <td>${uni.nomDeUni}</td>
+              <td>${uni.cRes}</td>
+              <td>${uni.cNRes}</td>
+              <td>${Math.round((uni.cRes * 100) / (uni.cNRes + uni.cRes))}%
+              </td>
+            </tr>`
+            )
+          );
+        } else alert("No logramos encontrarlo");
+    }
+    e.target.reset();
+  });
+});
